3a1ca377e50ba504ba28a02d4de44f3e060da162
lekro <kapuraya@gmail.com>
2017-10-10 13:19:42 -0500

    Add recalculation of all elo scores

--- cogs/elo.py
+++ cogs/elo.py
@@ -29,14 +29,35 @@ class Elo:
             # Create new user status
             self.user_status = pd.DataFrame(columns=['elo', 'wins', 'losses'])
 
-    async def get_elo(self, player):
+    def get_elo(self, user_status, player):
         try:
-            return self.user_status.loc[player, 'elo']
+            return user_status.loc[player, 'elo']
         except:
-            self.user_status.loc[player] = (self.config['default_elo'], 0, 0)
+            user_status.loc[player] = (self.config['default_elo'], 0, 0)
             return self.config['default_elo']
 
-    async def update_players(self, match_df):
+    async def recalculate_elo(self):
+        
+        # Reinstantiate user status
+        user_status = pd.DataFrame(columns=self.user_status.columns)
+        # Reinstantiate match history
+        match_history = pd.DataFrame(columns=self.match_history.columns)
+
+        # For each match...
+        for time, match in self.match_history.groupby('timestamp'):
+            match = match.copy()
+            # Replace the elo rating of each player with what it should be, from the user_status
+            match['elo'] = match['playerID'].applymap(lambda p: self.get_elo(user_status, p))
+        
+            # Update the user status for each player
+            await self.update_players(match, user_status)
+
+        # Finally, update the Elo object's match history and user status
+        self.match_history = match_history
+        self.user_status = user_status
+            
+
+    async def update_players(self, match_df, user_status):
         team_elo = match_df.groupby('team')[['elo', 'status']].sum(numeric_only=True)
 
         score_diff = team_elo.iloc[0, 'elo'] - team_elo.iloc[1, 'elo']
@@ -49,17 +70,17 @@ class Elo:
         team_elo['expected'] = 1./(1.+10.**(team_elo['other_elo'] - team_elo['elo']))
 
         # Actual team scores
-        team_elo['actual'] = team_elo['status'].head(1).applymap(self.get_status_value).tolist()
+        team_elo['actual'] = team_elo['status'].applymap(self.get_status_value)
         k_factor = self.config['k_factor']
         team_elo['elo_delta'] = k_factor * (team_elo['actual'] - team_elo['expected'])
         
         for index, row in match_df.iterrows():
-            self.user_status.loc[player, 'elo'] += team_elo.loc[row.team, 'elo_delta']
+            user_status.loc[player, 'elo'] += team_elo.loc[row.team, 'elo_delta']
             actual_score = team_elo.loc[row.team, 'actual']
             if actual_score == 1:
-                self.user_status.loc[player, 'wins'] += 1
+                user_status.loc[player, 'wins'] += 1
             elif actual_score == 0:
-                self.user_status.loc[player, 'losses'] += 1
+                user_status.loc[player, 'losses'] += 1
 
 
     def get_status_value(self, status):
@@ -93,27 +114,31 @@ class Elo:
             team_status = team.pop()
             for team_member in team:
                 # Get player's elo
-                tm_elo = self.get_elo(team_member)
+                tm_elo = self.get_elo(self.user_status, team_member)
 
                 match_data.append([time_now, team_member, tm_elo, team_name, team_status])
                 
-                # Update player's wins/losses 
-                if team_status == 'win':
-                    self.user_status.loc[player, 'wins'] += 1
-                elif team_status == 'loss':
-                    self.user_status.loc[player, 'losses'] += 1
         
         # Create the df
         match_df = pd.DataFrame(match_data, columns=self.match_history.columns)
 
-        # TODO update elo rating of player and wins/losses,
+        # update elo rating of player and wins/losses,
         # by calling another function
+        await self.update_players(match_df, self.user_status)
 
         # Add the new df to the match history
         self.match_history = self.match_history.append(match_df)
         # Sometimes there are circular references within dataframes? so we have to
         # invoke the gc
         gc.collect()
+        await self.bot.say('Added match!')
+        await self.bot.say(str(match_df))
+
+    @bot.command(pass_context=True)
+    async def recalculate(self, ctx):
+        '''Recalculate elo ratings from scratch.'''
+        await recalculate_elo(self)
+        await self.bot.say('Recalculated elo ratings!')
 
 
 
